c3e438959c10ad8994fb42f2381b42356e38e4a6
Giampiero Porfiri <72532099+Giampiero-Porfiri@users.noreply.github.com>
2023-07-06 11:04:24 +0200

    Fixes

--- src/test/java/it/gov/pagopa/taxonomy/service/ServiceTest.java
+++ src/test/java/it/gov/pagopa/taxonomy/service/ServiceTest.java
@@ -4,34 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.taxonomy.constants.Version;
 import it.gov.pagopa.taxonomy.exception.AppException;
 import it.gov.pagopa.taxonomy.model.TaxonomyObjectStandard;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.swing.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(properties = { "taxonomy.jsonName=src/test/resources/taxonomy.json" })
-@AutoConfigureMockMvc
-
 class ServiceTest {
   @Autowired TaxonomyService taxonomyService;
 
@@ -51,11 +39,11 @@ class ServiceTest {
   }
     @Nested
     @TestPropertySource(properties = "taxonomy.jsonName=src/test/test/none.json")
-    public class TestWrongPath{
+    class TestWrongPath{
         @Test
         void getJsonWrongPath() {
             try {
-                List<? extends TaxonomyObjectStandard> standardList = taxonomyService.getTaxonomyList(Version.STANDARD.toString());
+                taxonomyService.getTaxonomyList(Version.STANDARD.toString());
             }catch(AppException exc) {
                 assertEquals(HttpStatus.NOT_FOUND, exc.getHttpStatus());
             }catch(Exception exc) {
@@ -66,11 +54,11 @@ class ServiceTest {
 
     @Nested
     @TestPropertySource(properties = "taxonomy.jsonName=src/test/resources/corrtaxonomy.json")
-    public class TestParsingError{
+    class TestParsingError{
         @Test
         void getJsonParsingError(){
             try {
-                List<? extends TaxonomyObjectStandard> standardList = taxonomyService.getTaxonomyList(Version.STANDARD.toString());
+                taxonomyService.getTaxonomyList(Version.STANDARD.toString());
             }catch(AppException exc) {
                 assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, exc.getHttpStatus());
             }catch(Exception exc) {
