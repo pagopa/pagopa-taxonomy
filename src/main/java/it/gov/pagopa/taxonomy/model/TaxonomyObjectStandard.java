720ced3ef4bff5ddbcbf402e069e350bec247540
Giampiero Porfiri <72532099+Giampiero-Porfiri@users.noreply.github.com>
2023-07-12 16:38:08 +0200

    NOD-363 Wildcard fix, extensions fix, jsonIgnore & more

--- src/main/java/it/gov/pagopa/taxonomy/model/TaxonomyObjectStandard.java
+++ src/main/java/it/gov/pagopa/taxonomy/model/TaxonomyObjectStandard.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.taxonomy.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -9,31 +10,11 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-public class TaxonomyObjectStandard {
-  @JsonProperty(value="CODICE TIPO ENTE CREDITORE")
-  private String codiceTipoEnteCreditore;
-  @JsonProperty(value="TIPO ENTE CREDITORE")
-  private String tipoEnteCreditore;
-  @JsonProperty(value="PROGRESSIVO MACRO AREA PER ENTE CREDITORE")
-  private String progressivoMacroAreaPerEnteCreditore;
-  @JsonProperty(value="NOME MACRO AREA")
-  private String nomeMacroArea;
-  @JsonProperty(value="DESCRIZIONE MACRO AREA")
-  private String descrizioneMacroArea;
-  @JsonProperty(value="CODICE TIPOLOGIA SERVIZIO")
-  private String codiceTipologiaServizio;
-  @JsonProperty(value="TIPO SERVIZIO")
-  private String tipoServizio;
-  @JsonProperty(value="MOTIVO GIURIDICO DELLA RISCOSSIONE")
-  private String motivoGiuridicoRiscossione;
-  @JsonProperty(value="DESCRIZIONE TIPO SERVIZIO")
-  private String descrizioneTipoServizio;
-  @JsonProperty(value="VERSIONE TASSONOMIA")
-  private String versioneTassonomia;
-  @JsonProperty(value="DATI SPECIFICI INCASSO")
-  private String datiSpecificiIncasso;
+public class TaxonomyObjectStandard extends TaxonomyObject{
+  @JsonIgnore
   @JsonProperty(value="DATA INIZIO VALIDITA")
   private String dataInizioValidita;
+  @JsonIgnore
   @JsonProperty(value="DATA FINE VALIDITA")
   private String dataFineValidita;
 }
