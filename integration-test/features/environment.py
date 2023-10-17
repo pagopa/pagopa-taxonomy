2331a7036114d3585bc8b993b3611f80f604c471
Andrea Barchi <andrea.barchi@tasgroup.eu>
2023-10-17 11:19:40 +0200

    Added config.json

--- integration-test/features/environment.py
+++ integration-test/features/environment.py
@@ -1,21 +1,29 @@
 import os
+import json
 from azure.storage.blob import ContainerClient
 
-connectionString = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
-storageContainer = 'input'
 parent_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
 csv_file_path = os.path.join(parent_directory, 'taxonomy.csv')
 
+def before_all(context):
+    # initialize precondition cache to avoid check systems up for each scenario
+    context.precondition_cache = set()
+
+    print('Global settings...')
+
+    more_userdata = json.load(open(os.path.join(context.config.base_dir + "/config/config.json")))
+    context.config.update_userdata(more_userdata)
+
 # before feature
 def before_feature(context, feature):
-    container = ContainerClient.from_connection_string(connectionString, storageContainer)
+    container = ContainerClient.from_connection_string(context.config.userdata.get("global_config").get("connection_string"), context.config.userdata.get("global_config").get("input_container_name"))
     try:
         if not container.exists():
             container.create_container()
             print("Container created.")
-        if not container.get_blob_client("taxonomy.csv").exists():
+        if not container.get_blob_client(context.config.userdata.get("global_config").get("csv_blob_name")).exists():
             with open(file=csv_file_path, mode="rb") as data:
-                container.upload_blob(name="taxonomy.csv", data=data)
+                container.upload_blob(name=context.config.userdata.get("global_config").get("csv_blob_name"), data=data)
                 print("Blob Uploaded.")
     except Exception as e:
         print(e)
--- integration-test/features/steps/steps.py
+++ integration-test/features/steps/steps.py
@@ -2,22 +2,20 @@ import requests
 from behave import *
 import os
 from azure.storage.blob import ContainerClient
+import utils
 
-connection_string = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
-input_container_name = 'input'
-output_container_name = 'output'
 parent_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
 csv_file_path = os.path.join(parent_directory, 'taxonomy.csv')
 
 @given('the infrastructure is up and running')
 def step_impl(context):
-    url='http://localhost:7071/info'
+    url = utils.get_global_conf(context, "url")+'/info'
     response = requests.get(url, verify=False)
     assert response.status_code == 200
 
 @when('someone requests the taxonomy to be generated')
 def step_impl(context):
-    url = 'http://localhost:7071/generate'
+    url = utils.get_global_conf(context, "url")+'/generate'
     response = requests.get(url, verify = False)
     setattr(context, 'response', response)
 
@@ -31,7 +29,7 @@ def step_impl(context):
 
 @when('a partner requests a taxonomy {extension} as {version} version')
 def step_impl(context, extension, version):
-    url = 'http://localhost:7071/taxonomy'
+    url = utils.get_global_conf(context, "url")+'/taxonomy'
     Params={}
     Params['version'] = version
     Params['extension'] = extension
@@ -41,24 +39,24 @@ def step_impl(context, extension, version):
 @given('There is no {file_ext} file in the storage')
 def step_impl(context, file_ext):
     if file_ext.lower() == 'csv':
-      input_container = ContainerClient.from_connection_string(connection_string, input_container_name)
+      input_container = ContainerClient.from_connection_string(utils.get_global_conf(context, "connection_string"), utils.get_global_conf(context, "input_container_name"))
       try:
           if not input_container.exists():
               input_container.create_container()
               print("Container created.")
-          if input_container.get_blob_client("taxonomy.csv").exists():
-              input_container.delete_blob(blob="taxonomy.csv")
+          if input_container.get_blob_client(utils.get_global_conf(context, "csv_blob_name")).exists():
+              input_container.delete_blob(blob=utils.get_global_conf(context, "csv_blob_name"))
               print("Deleted taxonomy." + file_ext)
       except Exception as e:
           print(e)
     elif file_ext.lower() == 'json':
-      output_container = ContainerClient.from_connection_string(connection_string, output_container_name)
+      output_container = ContainerClient.from_connection_string(utils.get_global_conf(context, "connection_string"), utils.get_global_conf(context, "output_container_name"))
       try:
           if not output_container.exists():
               output_container.create_container()
               print("Container created.")
-          if output_container.get_blob_client("taxonomy.json").exists():
-              output_container.delete_blob(blob="taxonomy.json")
+          if output_container.get_blob_client(utils.get_global_conf(context, "json_blob_name")).exists():
+              output_container.delete_blob(blob=utils.get_global_conf(context, "json_blob_name"))
               print("Deleted taxonomy." + file_ext)
       except Exception as e:
           print(e)
